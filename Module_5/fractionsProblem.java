3045bba5405c267d64290ab14857e3fb4d0477f9
jimmyleast <56903583+jimmyleast@users.noreply.github.com>
2021-11-14 18:29:36 -0600

    new files

---
+++ Module_5/fractionsProblem.java
@@ -0,0 +1,48 @@
+public class fractionsProblem {
+    public static void main(String[] args) {
+
+        //declaring my variables
+        float fResult = 0;
+        float lResult =0;
+        String fraction;
+        float total;
+        int startingDenominator = 3;
+        int endingDenominator = 99;
+        int numerator = 1;
+
+
+        //explaining to the user which part we will show in this output
+        System.out.println("\n Showing the fractions in Ascending order with their results\n ");
+
+        while (startingDenominator <= 99) {
+            //build all outputs for the print line to display them
+            fResult = (numerator/ (float) startingDenominator);
+            total = (lResult += fResult );
+
+            //building the fraction
+            fraction = (numerator + "/" + startingDenominator);
+
+            //print out the results to the user
+            System.out.println(fraction + "   Result:" + fResult +"   Running total:" + total );
+            startingDenominator++;
+        }
+
+        //explaining the loop finished
+        System.out.println("\n  Ascending loop finished");
+
+        //explaining to the user which part we will show in this output
+        System.out.println("\n Showing the fractions in Descending order with their results \n");
+        while (endingDenominator >= 3) {
+            //build all outputs for the print line to display them
+            fResult = (numerator/ (float) endingDenominator);
+            total = (lResult += fResult );
+
+            //building the fraction
+            fraction = (numerator + "/" + endingDenominator);
+            System.out.println(fraction + "   Result:" + fResult +"   Running total:" + total);
+            endingDenominator--;
+        }
+        //explaining the loop finished
+        System.out.println("\n Descending loop finished");
+    }
+}
